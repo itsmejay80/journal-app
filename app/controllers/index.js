0c9260d58b1d22272bf43ae8dddde3f9cf4da985
Jayesh Patel <40144196+itsmejay80@users.noreply.github.com>
2020-09-29 15:11:04 +0530

    model sorting

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,9 @@
 import Controller from "@ember/controller";
+import { sort } from "@ember/object/computed";
+import EmberResolver from "ember-resolver";
 
-export default Controller.extend({});
+export default Controller.extend({
+  //   blogs: this.model,
+  //   blogSortDefination: ["date:desc"],
+  //   sortedBlogs: sort("blogs", "blogSortDefinition"),
+});
